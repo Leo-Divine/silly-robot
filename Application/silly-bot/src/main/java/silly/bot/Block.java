137e455bb2ce76bcd942d7f308d175128bfa0bca
Leo <isidororossinia@gmail.com>
2025-09-29 13:55:05 -0400

    Worked more on Java, need to get Images working

--- Application/silly-bot/src/main/java/silly/bot/Block.java
+++ Application/silly-bot/src/main/java/silly/bot/Block.java
@@ -1,18 +1,29 @@
 package silly.bot;
 
 enum BlockType {
-    Wait,
-    SetSpeed,
-    Move,
-    RotateLeft,
-    RotateRight,
-    SetLeftColor,
-    SetRightColor
+    Wait(""),
+    SetSpeed(""),
+    Move(""),
+    RotateLeft(""),
+    RotateRight(""),
+    SetLeftColor(""),
+    SetRightColor("");
+
+    public final String image;
+
+    private BlockType(String image) {
+        this.image = image;
+    }
 }
 
 public class Block {
-    BlockType blockType;
-
+    private int id;
+    private BlockType blockType;
+    private int aboveBlock;
+    private int belowBlock;
+    private double xPos = 0.0;
+    private double yPos = 0.0;
+    
     public Block(BlockType b) {
         blockType = b;
     }
--- Application/silly-bot/src/main/java/silly/bot/Editor.java
+++ Application/silly-bot/src/main/java/silly/bot/Editor.java
@@ -2,7 +2,11 @@ package silly.bot;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+
+import java.io.File;
+import java.net.URL;
 import java.util.Vector;
 
 public class Editor extends Canvas {
@@ -11,6 +15,9 @@ public class Editor extends Canvas {
 
     public Editor(int i, int j) {
         super(i, j);
+        blocks.add(new Block(
+            BlockType.SetSpeed
+        ));
     }
 
     public void drawBackground() {
@@ -18,7 +25,10 @@ public class Editor extends Canvas {
     }
 
     public void drawBlocks() {
-        
+        File file = new File("C:/Users/isido/Documents/GitHub/silly-robot/resources/temp.png");
+        Image test = new Image(file.toURI().toString());
+        System.out.println(imageUrl.toExternalForm());
+        gc.drawImage(test, 75, 75, 300, 400);
     }
 
     public void test() {
