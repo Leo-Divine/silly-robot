6c51ed285eb2d2e8ebc58a5a88bd04acc74cc013
Leo <isidororossinia@gmail.com>
2025-09-29 06:45:47 -0400

    Trying to get everything to work

--- Arduino/src/main.rs
+++ Arduino/src/main.rs
@@ -3,9 +3,15 @@
 
 extern crate alloc;
 
-use arduino_hal::{prelude::*, spi, Peripherals, Pins};
+use arduino_hal:: {Peripherals, Pins};
 use panic_halt as _;
-use crate::block::Command;
+//use crate::block::{Command, Action, Block, BlockType};
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 mod block;
 
@@ -14,17 +20,43 @@ fn main() -> ! {
     let dp: Peripherals = arduino_hal::Peripherals::take().unwrap();
     let pins: Pins = arduino_hal::pins!(dp);
 
-    let command: Command = Command::new() ;
-    let is_program_running = false;
+    /* 
+    let command: Command = Command::new(
+        Action::START,
+        &mut [
+            Block::new(
+                BlockType::Move,
+                [ 255, 0, 0 ]
+            )
+        ]
+    );
+
+    let mut is_program_running = false;
+    let mut lineCount: u32 = 0;
+    */
 
     // set up serial interface for text output
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
-
-    let mut buzzer = pins.a0.into_output();
-    
+    let mut light = pins.a0.into_output().downgrade();
 
     loop {
-        buzzer.toggle();
+        /*
+        // Check if Command is START or STOP and act Accordingly
+        if command.action == Action::STOP { lineCount = 0; }
+        is_program_running = command.action == Action::START;
+
+        // Continue if Program isn't Running
+        if !is_program_running { continue; }
+
+        // Set lineCount if 0
+
+        // Loop Through Blocks
+        for block in command.blocks.into_iter() {
+            block.run_block();
+        }
+        */
+
+        light.toggle();
         arduino_hal::delay_ms(1000);
     }
 }
