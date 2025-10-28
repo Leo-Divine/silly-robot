f69ccbfd6e8a503998e1236b6a020bd62af10ccb
Leo <isidororossinia@gmail.com>
2025-10-28 12:56:45 -0400

    🟣 style: Moved all server code to a new file, cleaned up code, and started documentation.

--- Application Comms/server/src/main/java/com/example/App.java
+++ Application Comms/server/src/main/java/com/example/App.java
@@ -8,26 +8,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import java.net.*;
 import java.io.*;
-
-enum ResponseCode {
-  OK,
-  DISCONNECT;
-}
-
-enum RobotCommand {
-  MOVE_FORWARD,
-  ROTATE_RIGHT,
-  ROTATE_LEFT,
-  SET_COLOR;
-}
 
 public class App extends Application {
   private Server server;
   private boolean isConnected = false;
 
-  private static Pane root = new Pane();;
+  private static Pane root = new Pane();
   private static Button btn_rotateRight;
   private static Button btn_rotateLeft;
   private static Button btn_moveForward;
@@ -40,30 +27,18 @@ public class App extends Application {
     AnimationTimer timer = new AnimationTimer() {
       @Override
       public void handle(long arg0) {
-        try {
-          if(!isConnected) { server.findClient(); isConnected = true; }
-
-          if(server.isMessageAvailable()) {
-            if (server.getMessage() == null) {
-              server.disconnectClient();
-              server.findClient();
-              return;
-            } 
-
-            if(server.readMessage() == ResponseCode.DISCONNECT) {
-              server.disconnectClient();
-              server.findClient();
-            }
-          }
-        } catch (IOException e) {
-          System.out.println("SERVER ERROR: " + e.getMessage());
-        }
+        // Handle Server Connection and Messages
+        try { handleServerConnection(); }
+        catch (IOException e) { System.out.println("SERVER ERROR: " + e.getMessage()); }
       }
     };
     timer.start();
 
+    // Create UI and Event Handlers
     createInterface();
     createEventHandlers();
+    
+    stage.setTitle("silly-bot-ide");
     stage.setScene(new Scene(root, 1200, 800));
     stage.show();
   }
@@ -130,84 +105,35 @@ public class App extends Application {
 
   }
 
+  private void handleServerConnection() throws IOException {
+    // Connect if not Already
+    if(!isConnected) {
+      server.findClient();
+      isConnected = true;
+    }
+
+    // Check if a Message has Been Recieved
+    if(server.isMessageAvailable()) {
+      // Restart Connection if NULL Messages are Being Recieved
+      if (server.getMessage() == null) {
+        server.disconnectClient();
+        isConnected = false;
+        server.findClient();
+        isConnected = true;
+        return;
+      } 
+
+      // Restart Connection if Client Disconnects
+      if(server.readMessage() == ResponseCode.DISCONNECT) {
+        server.disconnectClient();
+        isConnected = false;
+        server.findClient();
+        isConnected = true;
+      }
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     launch();
-  }
-}
-
-class Server {
-  private ServerSocket serverSocket;
-  private Socket clientSocket;
-  private PrintWriter serverOutput;
-  public BufferedReader serverInput;
-  public String inputLine;
-
-  public Server() throws IOException {
-    serverSocket = new ServerSocket(9090);
-  }
-
-  public void findClient() throws IOException {
-    System.out.println("SERVER: Server waiting for client...");
-    clientSocket = serverSocket.accept();
-    System.out.println("SERVER: A client has been connected (" + clientSocket.getInetAddress().getHostName() + ")!");
-
-    // Set up Communication
-    serverOutput = new PrintWriter(clientSocket.getOutputStream(), true);
-    serverInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-  }
-
-  public void disconnectClient() throws IOException {
-    System.out.println("Disconnecting");
-    serverOutput.close();
-    serverInput.close();
-    clientSocket.close();
-  }
-
-  public void sendCommand(RobotCommand command, int parameters[]) throws IOException {
-    switch(command) {
-      case MOVE_FORWARD:
-        serverOutput.println("R_000" + String.format("%03d", parameters[0]) + String.format("%03d", parameters[1]));
-        break;
-      case ROTATE_LEFT:
-        serverOutput.println("R_001");
-        break;
-      case ROTATE_RIGHT:
-        serverOutput.println("R_002");
-        break;
-      case SET_COLOR:
-        serverOutput.println(
-          "R_003" +
-          String.format("%03d", parameters[0]) +
-          String.format("%03d", parameters[1]) +
-          String.format("%03d", parameters[2]) +
-          String.format("%03d", parameters[3]) +
-          String.format("%03d", parameters[4]) +
-          String.format("%03d", parameters[5])
-          );
-        break;
-    }
-  }
-
-  public boolean isMessageAvailable() throws IOException {
-    return serverInput.ready();
-  }
-
-  public String getMessage() throws IOException {
-    inputLine = serverInput.readLine();
-    return inputLine;
-  }
-
-  public ResponseCode readMessage() throws IOException {
-    if ("FCKOFF".equals(inputLine)) {
-      return ResponseCode.DISCONNECT;
-    }
-    System.out.println(inputLine);
-    return ResponseCode.OK;
-  }
-
-  public void stopServer() throws IOException {
-    System.out.println("SERVER: Shutting down server.");
-    disconnectClient();
-    serverSocket.close();
   }
 }
