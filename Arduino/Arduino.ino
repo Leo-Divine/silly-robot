8d1c3bd579505831d52fca61b8734c4d8edfd58e
Leo <isidororossinia@gmail.com>
2025-11-20 16:42:27 -0500

    yyyy

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -9,7 +9,7 @@ SoftwareSerial Serial1(2, 3); // RX, TX
 char ssid[] = "IT-Shop";
 char pass[] = "B0n_J0v!";
 int status = WL_IDLE_STATUS;
-char server[] = "192.168.0.52";
+char server[] = "192.168.0.90";
 unsigned long lastConnectionTime = 0;
 const unsigned long postingInterval = 60000L; // 1 Min
 
@@ -63,19 +63,25 @@ void loop()
     if(c.substring(0, 5) == "R_000") {
       driving = Direction::FORWARD;
       speed = c.substring(5, 8).toInt();
-      lastHeading = 2;
     } else if(c.substring(0, 5) == "R_001") {
       driving = Direction::LEFT;
       speed = c.substring(5, 8).toInt();
-      lastHeading = 272;
+
+      lastHeading -= 20;
+      if(lastHeading < 0) {
+        lastHeading = 340;
+      }
     } else if(c.substring(0, 5) == "R_002") {
       driving = Direction::BACKWARD;
       speed = c.substring(5, 8).toInt();
-      lastHeading = 182;
     } else if(c.substring(0, 5) == "R_003") {
       driving = Direction::RIGHT;
       speed = c.substring(5, 8).toInt();
-      lastHeading = 92;
+      
+      lastHeading += 20;
+      if(lastHeading > 360) {
+        lastHeading = 20;
+      }
     } else if(c.substring(0, 5) == "R_004") {
       driving = Direction::STOPPED;
     } else if(c.substring(0, 5) == "R_006") {
@@ -88,24 +94,11 @@ void loop()
     c = "";
   }
 
-
   sphero.drive(speed, lastHeading);
-  switch(driving) {
-    case FORWARD:
-      sphero.drive(speed, 0);
-      break;
-    case BACKWARD:
-      sphero.drive(speed, 180);
-      break;
-    case LEFT:
-      sphero.drive(speed, 270);
-      break;
-    case RIGHT:
-      sphero.drive(speed, 90);
-      break;
-    case STOPPED:
-      sphero.stop(lastHeading);
-      break;
+  if (driving == Direction::STOPPED) {
+    sphero.stop(lastHeading);
+  } else {
+    sphero.drive(speed, lastHeading);
   }
   
   if (millis() - lastConnectionTime > postingInterval) {
